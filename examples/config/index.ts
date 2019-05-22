139a2c23028a4e1d3303f3b31193abcfa4a72d46
loogeek <xiaolong3956@gmail.com>
2019-05-22 23:35:22 +0800

    扩展axios.create静态接口

--- examples/config/index.ts
+++ examples/config/index.ts
@@ -17,7 +17,32 @@ import { AxiosTransformer } from '../../src/types';
 //   console.log(res.data);
 // });
 
-axios({
+// axios({
+//   transformRequest: [
+//     function(data) {
+//       return qs.stringify(data);
+//     },
+//     ...(axios.defaults.transformRequest as AxiosTransformer[])
+//   ],
+//   transformResponse: [
+//     ...(axios.defaults.transformResponse as AxiosTransformer[]),
+//     function(data) {
+//       if (typeof data === 'object') {
+//         data.b = 2;
+//       }
+//       return data;
+//     }
+//   ],
+//   url: '/config/post',
+//   method: 'post',
+//   data: {
+//     a: 1
+//   }
+// }).then(res => {
+//   console.log(res.data);
+// });
+
+const instance = axios.create({
   transformRequest: [
     function(data) {
       return qs.stringify(data);
@@ -32,7 +57,10 @@ axios({
       }
       return data;
     }
-  ],
+  ]
+});
+
+instance({
   url: '/config/post',
   method: 'post',
   data: {
--- src/axios.ts
+++ src/axios.ts
@@ -1,17 +1,22 @@
 import Axios from './core/Axios';
-import { AxiosInstance, AxiosRequestConfig } from './types';
+import { AxiosStatic, AxiosRequestConfig } from './types';
 import { extend } from './helpers/util';
 import defaults from './default';
+import mergeConfig from './core/mergeConfig';
 
-function createInstance(initConfig: AxiosRequestConfig): AxiosInstance {
+function createInstance(initConfig: AxiosRequestConfig): AxiosStatic {
   const context = new Axios(initConfig);
   const instance = Axios.prototype.request.bind(context);
 
   extend(instance, context);
 
-  return instance as AxiosInstance;
+  return instance as AxiosStatic;
 }
 
 const axios = createInstance(defaults);
+
+axios.create = function(config) {
+  return createInstance(mergeConfig(defaults, config));
+};
 
 export default axios;
